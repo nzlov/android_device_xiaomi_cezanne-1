5448bd9778f284428585f46db88c77f822ade416
ZhcnPanda <2623493447@qq.com>
2022-02-14 00:07:16 +0800

    cezanne: Clean Overlay

--- device.mk
+++ device.mk
@@ -22,7 +22,7 @@ PRODUCT_COMPATIBLE_PROPERTY_OVERRIDE := true
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-aosp
+    $(LOCAL_PATH)/overlay
 
 # Overlays -- Override vendor ones
 PRODUCT_PACKAGES += \
